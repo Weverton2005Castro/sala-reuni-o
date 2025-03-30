5d2eb22ab8c1d16cfeff104ac556448a0c2768fb
WevertonBlanco <wblanco846@gmail.com>
2025-03-30 20:13:41 -0300

    h

--- teste.js
+++ teste.js
@@ -5,24 +5,24 @@ function bookRoom() {
     const roomName = document.getElementById("roomName").value;
     const startTime = document.getElementById("startTime").value;
     const endTime = document.getElementById("endTime").value;
-    
+
     if (!userName || !roomName || !startTime || !endTime) {
         alert("Preencha todos os campos");
         return;
     }
-    
-    const conflict = bookings.some(booking => 
-        booking.roomName === roomName && 
-        ((startTime >= booking.startTime && startTime < booking.endTime) || 
-         (endTime > booking.startTime && endTime <= booking.endTime) || 
-         (startTime <= booking.startTime && endTime >= booking.endTime))
+
+    const conflict = bookings.some(booking =>
+        booking.roomName === roomName &&
+        ((startTime >= booking.startTime && startTime < booking.endTime) ||
+            (endTime > booking.startTime && endTime <= booking.endTime) ||
+            (startTime <= booking.startTime && endTime >= booking.endTime))
     );
-    
+
     if (conflict) {
         alert("Já existe uma reunião agendada para este horário nesta sala.");
         return;
     }
-    
+
     bookings.push({ userName, roomName, startTime, endTime });
     localStorage.setItem("bookings", JSON.stringify(bookings));
     updateRooms();
@@ -45,9 +45,9 @@ function clearAllBookings() {
 function updateRooms() {
     const roomsDiv = document.getElementById("rooms");
     roomsDiv.innerHTML = "";
-    
-    let rooms = { "Sala 1": [], "Sala 2": [], "Sala 3": [], "Sala 4": [], "Sala 5": [] };
-    
+
+    let rooms = { "Auditorio Cheila": [], "Sala 1": [], "Sala 2": [], "Sala 3": [], "Sala 4": [] };
+
     bookings.forEach((booking, index) => {
         rooms[booking.roomName].push(
             `De ${new Date(booking.startTime).toLocaleString()} até ${new Date(booking.endTime).toLocaleString()} 
@@ -55,7 +55,7 @@ function updateRooms() {
             <button class='cancel-btn' onclick='cancelBooking(${index})'>Cancelar</button>`
         );
     });
-    
+
     for (let room in rooms) {
         let schedule = rooms[room].join("<br>") || "Sem agendamentos";
         roomsDiv.innerHTML += `<div class='schedule'><strong>${room}</strong><br>${schedule}</div>`;
